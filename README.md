631c169e3cbaf1e9dbf635534c91719219081025
Axolotl <samuelmachat@gmail.com>
2018-05-30 16:18:42 -0700

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+This project was bootstrapped with [Create React App](https://github.com/facebookincubator/create-react-app).
+
+## About auth-demo
+This project was created to demonstrate the following functions on a React project: create user, login, post messages. <br>
+The stack consists of: postgres, sequelize, express, passport, create-react-app, reactstrap (bootstrap)
---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1 className="App-title">Welcome to [auth-demo]</h1>
+        </header>
+        <p className="App-intro">
+          And this is data and stuff ...
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
