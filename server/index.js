d28a3c4f769595d4cfda2b9e08412dcd8c7702ec
Axolotl <samuelmachat@gmail.com>
2018-06-14 14:28:30 -0700

    changed express solution so it's pointing to file instead of dir

--- server/index.js
+++ server/index.js
@@ -35,7 +35,7 @@ require("./routes")(app, passport);
 
 // handle front end routes other than "/"
 app.get('/*', (req, res) => {
-  res.sendFile(path.join(__dirname, "..", "build"), (err) => {
+  res.sendFile(path.join(__dirname, "..", "build/index.html"), (err) => {
     if (err) {
       res.status(500).send(err)
     }
